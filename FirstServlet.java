78e3f0678d08dfc6884da52f574a1124de93abaa
mahendra-16 <prajapatimahendra@ymail.com>
2016-12-26 19:08:08 +0530

    RequestDispatcher

---
+++ FirstServlet.java
@@ -0,0 +1,48 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if(username.equals("Mahendra") && password.equals("mahendra")){
+			RequestDispatcher rd = request.getRequestDispatcher("SecondServlet");
+			rd.forward(request, response);
+		}else{
+			out.println("Sorry you have entered wrong username or password.");
+			RequestDispatcher rd = request.getRequestDispatcher("kite.html");
+			rd.include(request, response);
+			
+		}
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ SecondServlet.java
@@ -0,0 +1,39 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out =response.getWriter();
+		
+		String username = request.getParameter("username");
+		out.println("Welcome " + username);
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
